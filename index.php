68b549362eaa9d933d8740922437661510c4952a
Athirson <athirsonarceus@gmail.com>
2022-04-11 16:18:20 -0300

    Dark mode

--- index.php
+++ index.php
@@ -8,10 +8,7 @@
     <title>PHP is bad</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
-    <video autoplay loop muted plays-inline>
-    <source src=".//..//video//astronaut.mp4" type="video/mp4">
-  </video>
+<body>  
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
   <li style="float:right; list-style: none;"><a class="active" href="#about">PHP SUCKS</a></li>
@@ -33,8 +30,7 @@
   </div>
 </nav>
 
-
-<!-- Gallery -->
+<button onclick="toggleDark()">Toggle dark mode</button><!-- Gallery -->
 <div class="container gallery">
 <?php 
 	if (isset($_COOKIE['filtro'])) {
@@ -820,31 +816,103 @@
   }
 ?>
 <!-- Gallery -->
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <!-- Footer -->
-<footer class="py-1 bg-dark text-center text-white">
-  <!-- Grid container -->
-  <div class="container p-4">
+<footer class="text-center text-lg-start bg-dark text-muted">
 
-    <!-- Section: Text -->
-    <section class="desc">
-      <p>
-        Gostaria de dizer que PHP não é legal não viu.
-      </p>
-    </section>
-    <!-- Section: Text -->
+  </section>
 
-  </div>
-  <!-- Grid container -->
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>PHP SUCKS
+          </h6>
+          <p>
+            Can i say that PHP isn't cool? So much trouble setting up all this stuff...
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Technologies
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">PHP</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">JavaScript</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Boostrap</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">HTML/ CSS</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Dark</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">City</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Nature</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">All</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i> São Paulo, SP 00000, BR</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            AthirsonArceus@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +55 11 95760-7177</p>
+          <p><i class="fas fa-print me-3"></i> +55 11 2511-3168</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
 
   <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2022 Copyright:
-    <a class="text-white" href="https://mdbootstrap.com/">php-é-muito-ruim.com.br</a>
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">https://gallery-filter.herokuapp.com/</a>
   </div>
   <!-- Copyright -->
 </footer>
 <!-- Footer -->
+<!-- Footer -->
 <script src=".//src//js//index.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script type="text/javascript" src=""></script>
 </body>
 </html>
